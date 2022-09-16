d8a9bfe9df1e49c28ec8cc1c4483c75c2343e3f2
Santosh Thakur <52118965+santosh2474@users.noreply.github.com>
2022-09-16 12:02:51 +0530

    Update Footer.js

--- client/src/component/Footer/Footer.js
+++ client/src/component/Footer/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => (
           View this project on{" "}
           <a
             className="profile"
-            href="https://github.com/arlbibek/dVoting"
+            href="https://github.com/santosh2474/Blockchain-Voting-System.git"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -23,7 +23,7 @@ const Footer = () => (
           Made with <i className="fas fa-heartbeat" /> by{" "}
           <a
             className="profile"
-            href="https://arlbibek.github.io"
+            href="https://github.com/santosh2474"
             target="_blank"
             rel="noopener noreferrer"
           >
